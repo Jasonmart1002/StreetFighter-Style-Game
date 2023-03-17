4f0d450069dcb9fe252b7cacfc53920f01140055
Jasonmart1002 <Jason.mart1002@gmail.com>
2023-03-17 14:54:21 -0400

    player blocks added

--- brawler/main.py
+++ brawler/main.py
@@ -1,4 +1,5 @@
 import pygame
+from fighter import Fighter
 
 pygame.init()
 
@@ -9,15 +10,39 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brawler")
 
+#load background image
+bg_image = pygame.image.load("assets/images/background/background.png").convert_alpha()
+
+#function for drawing backgound
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
+#create two instances of fighters
+fighter_1 = Fighter(200,310)
+fighter_2 = Fighter(700,310)
+
+
 #game loop
 run = True
 while run:
+
+    #draw background
+    draw_bg()
+
+    #draw fighters
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
 
 
     #event handler
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             run = False
+
+
+    #update display
+    pygame.display.update()
 
 
 #exit pygame
