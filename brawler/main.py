79d3a2fda51ac546a748a6868c229b40a01cde5f
Jasonmart1002 <Jason.mart1002@gmail.com>
2023-03-17 16:13:51 -0400

    sprites

--- brawler/main.py
+++ brawler/main.py
@@ -14,6 +14,12 @@ pygame.display.set_caption("Brawler")
 clock = pygame.time.Clock()
 FPS = 60
 
+#define colors
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+
 #load background image
 bg_image = pygame.image.load("assets/images/background/background.png").convert_alpha()
 
@@ -21,6 +27,13 @@ bg_image = pygame.image.load("assets/images/background/background.png").convert_
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
+
+#function for drawing fighter health bars
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
 #create two instances of fighters
 fighter_1 = Fighter(200,310)
@@ -35,6 +48,10 @@ while run:
 
     #draw background
     draw_bg()
+
+    #show player stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
 
     #move fighters
     fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
